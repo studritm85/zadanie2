31fcd7efb0d421464a7e72e4fcf5aa9a9f05e198
Ekaterina <studritm@gmail.com>
2024-05-01 15:08:46 +0400

    create

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialAmount = 100; // начальная сумма на счету
+        int depositAmount = 1100; // сумма пополнения
+
+        int bonus = 0;
+
+        if (depositAmount > 1000) {
+            bonus = (depositAmount - 1000) / 100;
+            System.out.println("Бонус: " + bonus + " рублей");
+        }
+
+        int totalAmount = initialAmount + depositAmount + bonus;
+
+        System.out.println("Итоговая сумма на счету: " + totalAmount + " рублей");
+    }
+}
+
+
